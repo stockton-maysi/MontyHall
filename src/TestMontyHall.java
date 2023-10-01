514ae3b711e49171992884f4fca542128c659d45
Ian Mays <maysi@go.stockton.edu>
2023-10-01 12:30:20 -0400

    Added answers to 2.20

--- src/TestMontyHall.java
+++ src/TestMontyHall.java
@@ -2,6 +2,15 @@
  * Runs the Monty Hall simulation 10,000 times each for switching and not switching
  * doors, and prints the approximate success rate in each scenario.
  * 
+ * Answers to 2.20:
+ * A. Events G, D1, and D2 each have a probability of 1/3, and so the contestant has
+ * a 1/3 chance of picking the door/curtain/whatever with the car.
+ * B-i. 1/3
+ * B-ii. Dud
+ * B-iii. Good prize
+ * B-iv. 2/3
+ * B-v. Switch to the other curtain
+ * 
  * @author Ian Mays
  */
 public class TestMontyHall {
